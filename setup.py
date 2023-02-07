ed6f1a50cd29f4796e6dd79a7b533b490a90d512
zen0831 <zenzenzen087@gmail.com>
2023-02-07 16:17:51 +0900

    test

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     url='https://twitter.com/qr1x_while :',
     description='ポケモンのタイプ相性を確認するライブラリです',
     long_description=readme,
-    long_description_content_type='text/markdown',"
+    long_description_content_type='text/markdown',
     license='MIT',
 
     # Package info
